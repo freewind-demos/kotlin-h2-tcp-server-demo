b52699ae7ee6ca8a78b4f1b6b99b186e5645071d
freewind <nowindlee@gmail.com>
2018-07-05 20:57:24 +0800

    tcp server demo

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
-Kotlin Hello World Demo
+Kotlin H2 Database Demo
 =======================
 
-Hello world from kotlin. Open it in IDE and run `Hello.kt`
+Run `Hello.kt` from IDE. 
+
+It will show the url to connect the memory h2 from web console, like:
+
+```
+jdbc:h2:tcp://192.168.1.102:9092/mem:mydb
+```
+
+Then run in terminal:
+
+```
+brew install h2
+h2
+```
+
+Fill the url and use username password `sa/sa` to connect.
+
+Another way to start h2 web console: Find you h2 jar file, and run
+
+```
+java -cp h2-1.4.197.jar org.h2.tools.Console -web -browser
+```
+
+Notice
+------
+
+It may be very very slow to connect the db in the web console, on Mac. The reason may be the host name of the machine defined by Mac.
+
+See: <https://stackoverflow.com/questions/41029630/h2-console-glacially-slow>
--- src/main/kotlin/example/Hello.kt
+++ src/main/kotlin/example/Hello.kt
@@ -1,7 +1,27 @@
 package example
 
-fun main(args: Array<String>) {
-    println(hello("Kotlin"))
-}
+import org.h2.tools.Server
+import java.sql.DriverManager
 
-fun hello(name: String): String = "Hello, $name!"
+private val DBNAME = "mydb"
+
+fun main(args: Array<String>) {
+    Class.forName("org.h2.Driver")
+
+    val conn = DriverManager.getConnection("jdbc:h2:mem:$DBNAME", "sa", "sa") // (2)
+    val stmt = conn.createStatement()
+    with(stmt) {
+        executeUpdate("create table mytbl(id int primary key, name varchar(255))")
+        executeUpdate("insert into mytbl values(1, 'Hello')")
+        executeUpdate("insert into mytbl values(2, 'World')")
+    }
+
+    val server = Server.createTcpServer().start()
+
+    println("Server started and connection is open. url: ")
+    println("jdbc:h2:" + server.getURL() + "/mem:" + DBNAME)
+
+    Thread.sleep(Long.MAX_VALUE)
+
+    server.stop()
+}
